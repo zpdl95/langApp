6a13b30455c8f794462a2b56400d51906818dea1
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2021-12-24 00:08:47 +0900

    #4.4 Our First Animation

--- App.js
+++ App.js
@@ -8,14 +8,11 @@ const Container = styled.View`
   align-items: center;
 `;
 
-/* 한번에 animated컴포넌트 생성 , 단점 import 해야함 */
-const Box = styled(Animated.createAnimatedComponent(TouchableOpacity))`
+const Box = styled.View`
   background-color: violet;
   width: 200px;
   height: 200px;
 `;
-
-/* const를 2번 사용한 animated컴포넌트 생성, 단점 생성을 2번해야함 */
 const AnimatedBox = Animated.createAnimatedComponent(Box);
 
 export default function App() {
@@ -23,10 +20,23 @@ export default function App() {
   /* Animated로 만든 값은 직접변경하지 않는다  */
   /* 모든 컴포넌트에 애니메이션을 만들 수 없다 */
   const Y = new Animated.Value(0);
-  const moveUp = () => {};
+  const moveUp = () => {
+    Animated.spring(Y, {
+      toValue: -200 /* Value가 얼만큼 커지거나 작아질지 선택 */,
+      useNativeDriver: true /* 애니메이션의 동작을 native가 관리함. 매초마다 native쪽으로 정보를 보내줄 필요가 없다 */,
+      bounciness: 50,
+    }).start();
+  };
+  Y.addListener((e) => console.log(e));
   return (
     <Container>
-      <Box onPress={moveUp} style={{ transform: [{ translateY: Y }] }}></Box>
+      <TouchableOpacity onPress={moveUp}>
+        <AnimatedBox style={{ transform: [{ translateY: Y }] }}></AnimatedBox>
+      </TouchableOpacity>
     </Container>
   );
 }
+
+/* Animated.decay() 초기속도로 시작해서 점점느려지고 멈춤 */
+/* Animated.spring() 물리 모델 제공 */
+/* Animated.timing() 일정시간동안 애니메이션 작동 */
