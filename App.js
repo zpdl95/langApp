7c2377a2f4b184c4df45585efcdf0add83161ed4
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2021-12-23 22:57:30 +0900

    #4.2 Manual Animations

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components/native";
 
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+const Box = styled.TouchableOpacity`
+  background-color: violet;
+  width: 200px;
+  height: 200px;
+`;
+
 export default function App() {
+  const [y, setY] = useState(0); /* y좌표값 저장 */
+  const [intervalId, setIntervalId] = useState(null); /* 인터벌 id 저장 */
+  const moveUp = () => {
+    const id = setInterval(
+      () => setY((prev) => prev + 1),
+      10
+    ); /* 매초 y값 변경 */
+    setIntervalId(id);
+  };
+  console.log("rendering...");
+  useEffect(() => {
+    /* y값이 200이 되면 인터벌 삭제 */
+    if (y === 200) {
+      clearInterval(intervalId);
+    }
+  }, [y, intervalId]);
   return (
-    <View>
-      <Text>hello</Text>
-    </View>
+    <Container>
+      <Box onPress={moveUp} style={{ transform: [{ translateY: -y }] }}></Box>
+    </Container>
   );
 }
