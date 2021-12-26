42e32d798e691de041735bee213c2f2542b589af
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2021-12-26 23:20:43 +0900

    #4.9 Pan Responder

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React, { useRef, useState } from "react";
-import { Animated, Dimensions } from "react-native";
+import React, { useRef } from "react";
+import { Animated, PanResponder } from "react-native";
 import styled from "styled-components/native";
 
 const Container = styled.View`
@@ -9,14 +9,12 @@ const Container = styled.View`
   background-color: tomato;
 `;
 
-const Box = styled.Pressable`
+const Box = styled.View`
   background-color: violet;
   width: 200px;
   height: 200px;
 `;
 const AnimatedBox = Animated.createAnimatedComponent(Box);
-
-const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 export default function App() {
   /* 애니메이션에 들어갈 값은 Animated로 만든다 */
@@ -24,39 +22,7 @@ export default function App() {
   /* 모든 컴포넌트에 애니메이션을 만들 수 없다 */
   /* useRef()는 다시 렌더링 될때마다 초기값으로 돌아가길 원하지 않는 value값을 기억함 .current속성을 가진 값에 넣어줌 */
   const POSITION = useRef(new Animated.ValueXY({ x: 0, y: 0 })).current;
-  const topLeft = Animated.timing(POSITION, {
-    toValue: {
-      x: -SCREEN_WIDTH / 2 + 100,
-      y: -SCREEN_HEIGHT / 2 + 100,
-    },
-    useNativeDriver: false,
-  });
-  const bottomLeft = Animated.timing(POSITION, {
-    toValue: {
-      x: -SCREEN_WIDTH / 2 + 100,
-      y: SCREEN_HEIGHT / 2 - 100,
-    },
-    useNativeDriver: false,
-  });
-  const bottomRight = Animated.timing(POSITION, {
-    toValue: {
-      x: SCREEN_WIDTH / 2 - 100,
-      y: SCREEN_HEIGHT / 2 - 100,
-    },
-    useNativeDriver: false,
-  });
-  const topRight = Animated.timing(POSITION, {
-    toValue: {
-      x: SCREEN_WIDTH / 2 - 100,
-      y: -SCREEN_HEIGHT / 2 + 100,
-    },
-    useNativeDriver: false,
-  });
-  const moveUp = () => {
-    Animated.loop(
-      Animated.sequence([topLeft, bottomLeft, bottomRight, topRight])
-    ).start();
-  };
+
   /* interpolate = 보간법: 두 지점을 주면 그 가운데 있는 지점을 추정하는것 */
   const borderRadius = POSITION.y.interpolate({
     inputRange: [-200, 200],
@@ -66,11 +32,20 @@ export default function App() {
     inputRange: [-200, 200],
     outputRange: ["red", "blue"],
   });
-
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true /* 모든 pressEvent를 감지 */,
+      /* 터치 움직임을 받는 함수 좌표를 넘겨줌 */
+      onPanResponderMove: (event, { dx, dy }) => {
+        POSITION.setValue({ x: dx, y: dy }); /* setValue = 직접 x,y값을 변경 */
+      },
+    })
+  ).current;
+  console.log("rendering...");
   return (
     <Container>
       <AnimatedBox
-        onPress={moveUp}
+        {...panResponder.panHandlers}
         style={{
           borderRadius,
           transform: [...POSITION.getTranslateTransform()],
