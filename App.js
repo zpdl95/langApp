8219d177fda5b870342aa13b345dd742f6c59bda
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2021-12-29 00:57:10 +0900

    #4.13 Card Project part Two

--- App.js
+++ App.js
@@ -21,44 +21,62 @@ const Card = styled(Animated.createAnimatedComponent(View))`
 `;
 
 export default function App() {
-  const panResponder = useRef(
-    PanResponder.create({
-      onStartShouldSetPanResponder: () => true,
-      onPanResponderGrant: () => {
-        onPressIn();
-      },
-      onPanResponderMove: (_, { dx }) => {
-        position.setValue(dx);
-      },
-      onPanResponderRelease: () => {
-        Animated.parallel([
-          onPressOut,
-          Animated.spring(position, {
-            toValue: 0,
-            useNativeDriver: true,
-          }),
-        ]).start();
-      },
-    })
-  ).current;
+  /* Values */
   const scale = useRef(new Animated.Value(1)).current;
   const position = useRef(new Animated.Value(0)).current;
-  const onPressIn = () =>
-    Animated.spring(scale, {
-      toValue: 0.9,
-      tension: 100,
-      useNativeDriver: true,
-    }).start();
+  const rotation = position.interpolate({
+    inputRange: [-250, 250],
+    outputRange: ["-30deg", "30deg"],
+    extrapolate:
+      "extend" /* input범위 밖으로 나갔을때 어떻게 처리할지 명시["clamp","extend","identity"] */,
+  });
+  /* Animations */
+  const onPressIn = Animated.spring(scale, {
+    toValue: 0.9,
+    tension: 100,
+    useNativeDriver: true,
+  });
   const onPressOut = Animated.spring(scale, {
     toValue: 1,
     useNativeDriver: true,
   });
+  const goCenter = Animated.spring(position, {
+    toValue: 0,
+    useNativeDriver: true,
+  });
+  /* PanResponders */
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderGrant: () => {
+        onPressIn.start();
+      },
+      onPanResponderMove: (_, { dx }) => {
+        position.setValue(dx);
+      },
+      onPanResponderRelease: (_, { dx }) => {
+        if (dx > 290 || dx < -290) {
+          Animated.spring(position, {
+            toValue: dx > 0 ? 500 : -500,
+            useNativeDriver: true,
+          }).start();
+        } else {
+          Animated.parallel([onPressOut, goCenter]).start();
+        }
+      },
+    })
+  ).current;
+
   return (
     <Container>
       <Card
         {...panResponder.panHandlers}
         style={{
-          transform: [{ scale }, { translateX: position }],
+          transform: [
+            { scale },
+            { translateX: position },
+            { rotateZ: rotation },
+          ],
         }}
       >
         <Ionicons name="pizza" color="#192a56" size={98} />
